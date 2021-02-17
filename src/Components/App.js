8da7fa3aba4fbc734af9dc563b8509ce808ec257
0x1S0L33T <scumonde@gmail.com>
2021-02-17 17:35:44 +0100

    Installed the App

---
+++ src/Components/App.js
@@ -0,0 +1,23 @@
+import React from "react"
+import './App.css'
+
+function App() {
+    return(
+        <div className="mainDiv">
+            <div className="firstDiv">
+                <input type="checkbox" />
+                <p className="p">PlaceHolder</p>    
+            </div>
+            <div className="secondDiv">
+                <input type="checkbox" />
+                <p>PlaceHolder</p>
+            </div>
+            <div className="thirdDiv">
+                <input type="checkbox" />
+                <p>PlaceHolder</p>
+            </div>
+        </div>
+    )
+}
+
+export default App
---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "./Header.css"
+
+function Header() {
+    return(
+        <div class="header">
+            <a href="/" class="logo">0x1S0L33T</a>
+        </div>
+    )
+}
+
+export default Header
